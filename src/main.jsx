c33e2f612b12089f28d5a73c6d84d871c6094d4e
Edilson Santos <edilson0982021@gmail.com>
2022-08-12 22:52:41 -0300

    Resolvendo conflitos.

--- src/main.jsx
+++ src/main.jsx
@@ -10,18 +10,9 @@ import { RecipesPage } from './Routes/RecipesPage/index'
 import { NutritionPage } from './Routes/NutritionPage/index'
 import { PartnersPage } from './Routes/PartnersPage/index'
 import { RegisterPage } from './Routes/RegisterPage/index'
-<<<<<<< HEAD
-import { LoginAdmPage } from './Routes/LoginAdmPage/index'
-import { PainelAdmPage } from './Routes/PainelAdmPage/index'
-import { PartnersAdmPage } from './Routes/PartnersAdmPage/index'
-import { RecipesAdmPage } from './Routes/RecipesAdmPage/index'
-import { RegisterPartnersAdmPage } from './Routes/RegisterPartnersAdmPage/index'
-import { RegisterRecipesAdmPage } from './Routes/RegisterRecipesAdmPage/index'
 import { ModalContact } from './Routes/ModalContact/index'
 import { ModalSuccess } from './Routes/ModalSuccess/index'
-=======
 import { RecipesAdmin } from './Routes/admin/RecipesAdmin/index'
->>>>>>> dev
 import { ScrollToTop } from './Routes/ScrollToTop';
 import { PartnersAdmin } from './Routes/admin/PartnersAdmin';
 import { LoginAdmPage } from './Routes/admin/LoginAdmPage';
@@ -35,22 +26,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path="receitas" element={<RecipesPage />} />
           <Route path="nutricao" element={<NutritionPage />} />
           <Route path="parceiros" element={<PartnersPage />} />
-<<<<<<< HEAD
-          <Route path="cadastro" element={<RegisterPage />} />
-          <Route path="login-adm" element={<LoginAdmPage />} />
-          <Route path="painel-adm" element={<PainelAdmPage />} />
-          <Route path="partners-adm" element={<PartnersAdmPage />} />
-          <Route path="recipes-adm" element={<RecipesAdmPage />} />
-          <Route path="register-partners-adm" element={<RegisterPartnersAdmPage />} />
-          <Route path="register-recipes-adm" element={<RegisterRecipesAdmPage />} />
           <Route path="modal-contact" element={<ModalContact />} />
           <Route path="modal-success" element={<ModalSuccess />} />
-=======
-          <Route path="cadastro" element={<RegisterPage />} />      
-          <Route path="login" element={<LoginAdmPage />} />           
-          <Route path="admin/receitas" element={<RecipesAdmin />} />        
-          <Route path="admin/parceiros" element={<PartnersAdmin />} />        
->>>>>>> dev
+          <Route path="cadastro" element={<RegisterPage />} />
+          <Route path="login" element={<LoginAdmPage />} />
+          <Route path="admin/receitas" element={<RecipesAdmin />} />
+          <Route path="admin/parceiros" element={<PartnersAdmin />} />
         </Routes>
       </ScrollToTop>
     </BrowserRouter>
