411825e7a84d367b0e08f9e1a62be4143b10db16
Edilson Santos <edilson0982021@gmail.com>
2022-08-12 23:30:20 -0300

    :refactor: change class name.

--- src/Routes/ModalContact/index.jsx
+++ src/Routes/ModalContact/index.jsx
@@ -4,19 +4,19 @@ import iconClose from '../../assets/images/icon-close.png';
 
 export function ModalContact() {
   return (
-    <section className='container'>
+    <section className='container__modal-contact'>
       <section className='modal-contact'>
-        <section className='close-modal' >
-          <section className='close-modal-icon' >
-            <img className='icon__close' src={iconClose} alt="" />
+        <section className='close-modal__modal-contact' >
+          <section className='close-modal-icon__modal-contact' >
+            <img className='icon__close__modal-contact' src={iconClose} alt="" />
           </section>
         </section>
         <h1 className='title-contact' >Gostaria de falar<br />com a gente?</h1>
         <form className='form-contact' action="">
-          <input className='input' placeholder='Deixe seu nome completo' type="text" name="" id="" />
-          <input className='input' placeholder='Deixe seu E-mail' type="text" name="" id="" />
-          <textarea placeholder='Deixe sua mensagem' name="" className='message' /* cols="30" rows="10" */></textarea>
-          <button className='button-contact' type="submit">Enviar</button>
+          <input className='input__modal-contact' placeholder='Deixe seu nome completo' type="text" name="" id="" />
+          <input className='input__modal-contact' placeholder='Deixe seu E-mail' type="text" name="" id="" />
+          <textarea placeholder='Deixe sua mensagem' name="" className='message__modal-contact' /* cols="30" rows="10" */></textarea>
+          <button className='button__modal-contact' type="submit">Enviar</button>
         </form>
       </section>
     </section>
--- src/Routes/ModalSuccess/index.jsx
+++ src/Routes/ModalSuccess/index.jsx
@@ -6,21 +6,21 @@ import logoGoVegan from '../../assets/images/logo-go-vegan.png';
 
 export function ModalSuccess() {
   return (
-    <section className='container'>
+    <section className='container__modal-success'>
       <section className='modal-success'>
-        <section className='close-modal' >
-          <section className='close-modal-icon' >
-            <img className='icon__close' src={iconClose} alt="" />
+        <section className='close-modal__success' >
+          <section className='background-icon-close__success' >
+            <img className='icon__close__modal-success' src={iconClose} alt="" />
           </section>
         </section>
         <section className='success__container' >
           <h1 className='title-success' >MENSAGEM ENVIADA<br /> COM SUCESSO</h1>
-          <section className='background-image-ok'>
-            <img className='icon-ok' src={iconOk} alt="" />
+          <section className='background-image-ok__modal-success'>
+            <img className='icon-ok__modal-success' src={iconOk} alt="" />
           </section>
           <h2 className='subtitle-success' >Obrigado pelo contato!</h2>
-          <section className='go-vegan-container' >
-            <img className='logo-go-vegan' src={logoGoVegan} alt="" />
+          <section className='go-vegan-container__modal-success' >
+            <img className='logo-go-vegan__modal-success' src={logoGoVegan} alt="" />
           </section>
         </section>
       </section>
