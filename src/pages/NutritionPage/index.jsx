b1c3b692cbaeb3a5d8f5f35ac1f11a8c0361a5e7
Juliana Sinnott <julianasinnott@outlook.com>
2022-08-14 19:31:56 -0300

    feat: responsive design nutrition page

--- src/pages/NutritionPage/index.jsx
+++ src/pages/NutritionPage/index.jsx
@@ -1,6 +1,7 @@
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
 import "./index.css";
+import "./responsive.css";
 import { SearchInput } from "../../components/SearchInput";
 import { FoodCard } from "./components/FoodCard";
 
