70556c920af01dbab227d27dd363b71bd44315ea
Juliana Sinnott <julianasinnott@outlook.com>
2022-08-15 00:17:33 -0300

    feat: implements search food funcionality

--- src/pages/NutritionPage/index.jsx
+++ src/pages/NutritionPage/index.jsx
@@ -1,34 +1,22 @@
-import { Footer } from "../../components/Footer";
-import { Header } from "../../components/Header";
 import "./index.css";
 import "./responsive.css";
+import { Footer } from "../../components/Footer";
+import { Header } from "../../components/Header";
 import { SearchInput } from "../../components/SearchInput";
 import { FoodCard } from "./components/FoodCard";
+import tacoTable from "../../features/NutritionPage/taco.json"
+import { useState } from "react";
 
 export function NutritionPage() {
-  const mockTable = [
-    {
-      Descricao: "Abacate, cru",
-      Energia: 96.1547,
-      Proteina: 1.23913,
-      Lipideos: 8.39666,
-      CHO: 6.03086,
-    },
-    {
-      Descricao: "Abacaxi, cru",
-      Energia: 48.32221,
-      Proteina: 0.85869,
-      Lipideos: 0.12333,
-      CHO: 12.33463,
-    },
-    {
-      Descricao: "Abacaxi, polpa, congelada",
-      Energia: 30.59179,
-      Proteina: 0.46666,
-      Lipideos: 0.11333,
-      CHO: 7.79866,
-    },
-  ];
+  const inicialFoodsArray = tacoTable.slice(0,25)
+  const [foodsArray, setFoodsArray] = useState(inicialFoodsArray)
+  function handleSearch(value) {
+    value ? filterFoods(value.toLowerCase()) : setFoodsArray(inicialFoodsArray)
+  }
+  function filterFoods(value) {
+    const filteredFoods = tacoTable.filter(food => food.Descricao.toLowerCase().includes(value))
+    setFoodsArray(filteredFoods.slice(0,25))
+  }
 
   return (
     <div>
@@ -41,12 +29,14 @@ export function NutritionPage() {
           Busque as informações nutricionais na Tabela TACO
         </h2>
         <p className="nutrition__info">
-          - Informações baseadas em 100g do alimento -
+          Informações baseadas em 100g do alimento 
         </p>
-        <SearchInput />
-        {mockTable.map((item, index) => (
+        <SearchInput handleSearch={handleSearch} />
+        {foodsArray.length > 0  ? foodsArray.map((item, index) => (
           <FoodCard key={index} tableItem={item} />
-        ))}
+        )) : (
+          <p> Nenhum alimento encontrado :( </p>
+        )}
       </main>
       <Footer />
     </div>
