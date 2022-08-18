076e739796fac59ab288e7cda1bd34e6808bf1df
Larissa Lavínia <larissa11.cedaspy@gmail.com>
2022-08-18 16:51:32 -0300

    Validando formulário

--- src/components/ModalContact/index.jsx
+++ src/components/ModalContact/index.jsx
@@ -2,8 +2,52 @@ import './index.css';
 import './responsive.css';
 
 import iconClose from '../../assets/images/icon-close.png';
+import * as React from 'react';
 
 export function ModalContact(props) {
+
+  const [form, setForm] = React.useState({
+    nome: '',
+    email: '',
+    mensagem: ''
+});
+
+const [erro, setErro] = React.useState("")
+
+const handleChange = (e) => {
+  setForm({
+    ...form,
+    [e.target.name]: e.target.value
+  });
+
+}
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  if(!validate()) return;
+
+  const saveForm = true;
+  
+  if (saveForm) {
+    console.log("enviado")
+    console.log(form)
+    };
+}
+
+function validate() {
+  if (form.nome > 30) {
+    return setErro("Máximo de 30 caracteres.")
+  } 
+  if(!form.email.includes("@")){
+    return setErro("E-mail inválido!")
+  }
+  if(form.mensagem > 300){
+    return setErro("Máximo de 300 caracteres.")
+  }
+
+  return true
+}
+
   return (
     <div className='modal-teste'>
       <section className='modal-contact'>
@@ -13,11 +57,39 @@ export function ModalContact(props) {
           </section>
         </section>
         <h1 className='title-contact' >Gostaria de falar<br />com a gente?</h1>
-        <form className='form-contact' action="">
-          <input className='input__modal-contact' placeholder='Deixe seu nome completo' type="text" name="" id="" />
-          <input className='input__modal-contact' placeholder='Deixe seu E-mail' type="text" name="" id="" />
-          <textarea placeholder='Deixe sua mensagem' name="" className='message__modal-contact' /* cols="30" rows="10" */></textarea>
-          <button className='button__modal-contact' type="submit">Enviar</button>
+        <form action="https://formsubmit.co/larissa11.cedaspy@gmail.com" method="POST"
+        className='form-contact'
+        
+        >
+          <input 
+          className='input__modal-contact' 
+          placeholder='Deixe seu nome completo'
+          required 
+          type="text" 
+          name="nome"
+          value={form.nome}
+          pattern="[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$"
+          onChange={handleChange}
+          />
+          <input 
+          className='input__modal-contact' 
+          placeholder='Deixe seu E-mail'
+          required 
+          type="text" 
+          name="email"
+          value={form.email}
+          onChange={handleChange}
+          />
+          <textarea 
+          placeholder='Deixe sua mensagem' 
+          required
+          name="mensagem" 
+          className='message__modal-contact'
+          value={form.mensagem}
+          onChange={handleChange}
+           /* cols="30" rows="10" */></textarea>
+          <button className='button__modal-contact' type="submit" onSubmit={handleSubmit}>Enviar</button>
+          <span className='spanErro'>{erro}</span>
         </form>
       </section>
     </div>
