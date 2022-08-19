8e8fbfd7bd62f7508f498707ffdc5d28221eaa10
Juliana Sinnott <julianasinnott@outlook.com>
2022-08-19 19:30:20 -0300

    feat: implements rendering of recipe information specific to the click a card

--- src/pages/RecipesPage/index.jsx
+++ src/pages/RecipesPage/index.jsx
@@ -7,29 +7,48 @@ import "./responsive.css";
 import { theme } from "../../utils/theme";
 import recipes from "../../features/RecipesPage/recipes.json"
 import { useEffect, useState } from "react";
+import { RecipeComplete } from "./components/RecipeComplete";
 
 export function RecipesPage() {
   const [type, setType] = useState('SALGADAS')
+  const [hasClick, setHasClick] = useState(false)
+  const [selectedRecipe, setSelectedRecipe] = useState('')
   function handleType(value) {
     setType(value)
   }
+  function handleClick(recipe) {
+    setHasClick(!hasClick)
+    setSelectedRecipe(recipe)
+  }
   const render = recipes.map((recipe, index) => (
     recipe.type === type &&
-    <Card
-      key={index}
-      item={recipe}
-      color={theme.colors.quaternary}
-      width={200}
-      height={250}
-     />
+      <Card
+        handleClick={() => handleClick(recipe)}
+        key={index}
+        item={recipe}
+        color={theme.colors.quaternary}
+        width={200}
+        height={250}
+      />
   ))
   useEffect(()=> {
     render
-  }, [type])          
+  }, [type])
+  
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [hasClick]);
  
   return (
+    <>      
     <div>
       <Header type={"USER"} />
+      {hasClick?
+      <RecipeComplete
+        handleClick={handleClick}
+        selectedRecipe={selectedRecipe}
+      />
+      :
       <main className="recipes__main">
         <h1 className="recipes__title">
           Escolha uma das opções abaixo e mostraremos para você seleções
@@ -45,7 +64,9 @@ export function RecipesPage() {
         </section>
         <Button value="Carregar mais" color={theme.colors.secundary} />
       </main>
+      }
       <Footer />
     </div>
+    </>
   );
 }
