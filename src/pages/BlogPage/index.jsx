0ca9aa8a5ec1c6fcc1e46b43b6a8f9349778f0d1
Juliana Sinnott <julianasinnott@outlook.com>
2022-08-18 23:34:04 -0300

    refactor: update props name

--- src/pages/BlogPage/index.jsx
+++ src/pages/BlogPage/index.jsx
@@ -53,7 +53,7 @@ export function BlogPage() {
           <Button
             value={loading ? 'Carregando...' : "Carregar mais"}
             color={theme.colors.tertiary}
-            handlePageSize={handlePageSize}
+            handleClick={handlePageSize}
           />
           {/* <div className="banner_blog">
             <h3 className="banner_title">Notícias mais recentes</h3>
