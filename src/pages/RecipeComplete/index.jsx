36887801e5031beebf5f2e869be0118f11e3989e
Juliana Sinnott <julianasinnott@outlook.com>
2022-08-20 23:48:15 -0300

    refactor: refactor recipes page and navigation to recipe info page

---
+++ src/pages/RecipeComplete/index.jsx
@@ -0,0 +1,39 @@
+import { Pizza, Cookie } from 'phosphor-react';
+import { Footer } from '../../components/Footer';
+import { Header } from '../../components/Header';
+import { useParams } from 'react-router-dom';
+import recipes from '../../features/RecipesPage/recipes.json'
+import './index.css';
+import './responsive.css';
+
+export function RecipeComplete() {
+
+  const { slug } = useParams()
+  const selectedRecipe = recipes.find(recipe => recipe.slug === slug)
+
+  return (
+    <>
+      <Header type={"USER"} />
+      <main className='recipe-complete'>
+        {selectedRecipe.type === 'DOCES'?
+          <Cookie size={55} className='recipe-complete__icon' />
+        :
+          <Pizza size={55} className='recipe-complete__icon' />
+        }
+        <div className='recipe-complete__image-container'>
+          <img className='recipe-complete__image' src={selectedRecipe.urlToImage} alt="" />
+        </div>
+        <h1 className='recipe-complete__title' >{selectedRecipe.title}</h1>
+        <section className='recipe-complete__preparation'>
+          <h2 className='recipe-complete__subtitle' >Ingredientes</h2>
+          <section className='recipe-complete__ingredients'>
+            <p className='recipe-complete__item'>{selectedRecipe.ingredients}</p>
+          </section>
+          <h2 className='recipe-complete__subtitle' >Modo de fazer</h2>
+          <p className='recipe-complete__preparation-mode'>{selectedRecipe.phases}</p>
+        </section>
+      </main>
+      <Footer />
+    </>
+  )
+}
--- src/routes.jsx
+++ src/routes.jsx
@@ -13,13 +13,14 @@ import { BlogPage } from './pages/BlogPage/index'
 import { NutritionPage } from './pages/NutritionPage/index'
 import { PartnersPage } from './pages/PartnersPage/index'
 import { RegisterPage } from './pages/RegisterPage/index'
-import { ModalSuccess } from './pages/ModalSuccess/index'
+// import { ModalSuccess } from './pages/ModalSuccess/index'
 import { RecipesAdmin } from './pages/admin/RecipesAdmin/index'
 import { ScrollToTop } from './pages/ScrollToTop';
 import { PartnersAdmin } from './pages/admin/PartnersAdmin';
 import { LoginAdmPage } from './pages/admin/LoginAdmPage';
 import { RegisterPartnersAdmin } from './pages/admin/RegisterPartnersAdmin';
 import { RegisterRecipesAdmin } from './pages/admin/RegisterRecipesAdmin';
+import { RecipeComplete } from './pages/RecipeComplete';
 
 function ProtectedRoutes({ redirectTo }) {
   const token = localStorage.getItem('token');
@@ -32,13 +33,13 @@ export function RoutesGoVegan() {
       <ScrollToTop>
         <Routes>
           <Route path="/" element={<App />} />
-          <Route path="receitas" element={<RecipesPage />} />
+          <Route path="receitas" element={<RecipesPage />} />          
+          <Route path="receitas/:slug" element={<RecipeComplete />} />          
           <Route path="blog" element={<BlogPage />} />
           <Route path="nutricao" element={<NutritionPage />} />
           <Route path="parceiros" element={<PartnersPage />} />
           <Route path="cadastro" element={<RegisterPage />} />
-          <Route path="modal-success" element={<ModalSuccess />} />
-          <Route path="cadastro" element={<RegisterPage />} />
+          {/* <Route path="modal-success" element={<ModalSuccess />} /> */}
           <Route path="login" element={<LoginAdmPage />} />
           <Route element={<ProtectedRoutes redirectTo='/' />}>
             <Route path="admin/receitas" element={<RecipesAdmin />} />
