042251f416dc035c720fe4d248e5ee3c338cf392
Juliana Sinnott <julianasinnott@outlook.com>
2022-08-22 14:37:40 -0300

    feat: integration with JSON server api in Recipes admin page

--- src/pages/admin/RecipesAdmin/index.jsx
+++ src/pages/admin/RecipesAdmin/index.jsx
@@ -1,21 +1,32 @@
 import { AdminTemplate } from "../../../components/templates/AdminTemplate";
 import { AdminSection } from "../../../components/templates/AdminTemplate/AdminSection";
 import '../../../components/templates/AdminTemplate/responsive.css';
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios"
 
 
 export function RecipesAdmin() {
+  const [recipes, setRecipes] = useState([])
+
+  useEffect(()=> {
+    async function getRecipes() {
+      try {
+        const response = await axios.get('https://go-vegan-api.herokuapp.com/recipes')
+        setRecipes(response.data)
+      }
+      catch (err) {
+        console.error(err);
+      }
+    }
+    getRecipes()
+  },[])
+
   return (
     <AdminTemplate colorRecipes='--tertiary' >
       <AdminSection
         title={'Receitas GoVegan'}
-        data={[
-          {
-            name: 'nome 1'
-          },
-          {
-            name: 'nome 2'
-          },
-        ]}
+        data={recipes}
       />
     </AdminTemplate>
   )
