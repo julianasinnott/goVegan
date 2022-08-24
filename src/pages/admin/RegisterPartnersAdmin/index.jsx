29823707bcf8d975d41d66cc524c90f80930ebc3
Edilson Santos <edilson0982021@gmail.com>
2022-08-24 17:06:23 -0300

    Modified in index.js, partners page.

--- src/pages/admin/RegisterPartnersAdmin/index.jsx
+++ src/pages/admin/RegisterPartnersAdmin/index.jsx
@@ -7,8 +7,8 @@ export function RegisterPartnersAdmin() {
     <AdminTemplate colorPartners='--tertiary'>
       <main className="main_RegisterPartners">
         <div className="content_RegisterPartners">
+          <h1 className="h1_RegisterPartners">Cadastrar novo parceiro</h1>
           <form className="form_RegisterPartners">
-            <h1 className="h1_RegisterPartners">Cadastrar novo parceiro</h1>
             <input className="input_RegisterPartners" type="text" placeholder="URL da imagem" />
             <input className="input_RegisterPartners" type="text" name="nome" placeholder="Título" />
             <input className="input_RegisterPartners" type="text" name="cidade" placeholder="Subtítulo" />
