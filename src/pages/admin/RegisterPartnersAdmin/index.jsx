0c9763c44ea721929bb29bd146fa3438c3f79b6b
Edilson Santos <edilson0982021@gmail.com>
2022-08-24 16:32:08 -0300

    Readjusting the order of the form.

--- src/pages/admin/RegisterPartnersAdmin/index.jsx
+++ src/pages/admin/RegisterPartnersAdmin/index.jsx
@@ -9,11 +9,11 @@ export function RegisterPartnersAdmin() {
         <div className="content_RegisterPartners">
           <form className="form_RegisterPartners">
             <h1 className="h1_RegisterPartners">Cadastrar novo parceiro</h1>
+            <input className="input_RegisterPartners" type="text" placeholder="URL da imagem" />
             <input className="input_RegisterPartners" type="text" name="nome" placeholder="Título" />
             <input className="input_RegisterPartners" type="text" name="cidade" placeholder="Subtítulo" />
             <input className="input_RegisterPartners" type="text" placeholder="Descrição" />
             <input className="input_RegisterPartners" type="text" placeholder="URL de contato" />
-            <input className="input_RegisterPartners" type="text" placeholder="URL da imagem" />
             <button className="btn_RegisterPartners" type="submit">Enviar</button>
           </form>
         </div>
