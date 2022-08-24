2bd98b82a60ccef925705af65908496f57c84ce1
Edilson Santos <edilson0982021@gmail.com>
2022-08-23 23:34:53 -0300

    Partners signup done.

--- src/pages/admin/RegisterPartnersAdmin/index.jsx
+++ src/pages/admin/RegisterPartnersAdmin/index.jsx
@@ -2,21 +2,21 @@ import { AdminTemplate } from "../../../components/templates/AdminTemplate";
 import "./style.css"
 
 export function RegisterPartnersAdmin() {
-    return(
-      <AdminTemplate colorPartners='--tertiary'>
-        <main className="main_RegisterPartners">
+  return (
+    <AdminTemplate colorPartners='--tertiary'>
+      <main className="main_RegisterPartners">
         <div className="content_RegisterPartners">
-         <form className="form_RegisterPartners">
-         <h1 className="h1_RegisterPartners">Cadastrar novo parceiro</h1>
-          <input className="input_RegisterPartners" type="text" name="nome" placeholder="Nome" />
-          <input className="input_RegisterPartners" type="text" name="cidade" placeholder="Cidade" />
-          <input className="input_RegisterPartners" type="text" placeholder="Endereço" />
-          <input className="input_RegisterPartners" type="text" placeholder="Contato" />
-          <input className="input_RegisterPartners" type="text" placeholder="Upload Imagem" />
-          <button className="btn_RegisterPartners" type="submit">Enviar</button>
-         </form>
+          <form className="form_RegisterPartners">
+            <h1 className="h1_RegisterPartners">Cadastrar novo parceiro</h1>
+            <input className="input_RegisterPartners" type="text" name="nome" placeholder="Título" />
+            <input className="input_RegisterPartners" type="text" name="cidade" placeholder="Subtítulo" />
+            <input className="input_RegisterPartners" type="text" placeholder="Descrição" />
+            <input className="input_RegisterPartners" type="text" placeholder="URL de contato" />
+            <input className="input_RegisterPartners" type="text" placeholder="URL da imagem" />
+            <button className="btn_RegisterPartners" type="submit">Enviar</button>
+          </form>
         </div>
       </main>
-      </AdminTemplate>
-    )
-  }
+    </AdminTemplate>
+  )
+}
