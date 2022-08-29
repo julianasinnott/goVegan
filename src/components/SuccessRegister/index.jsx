6cb4a169bc712fb8100c3be29bfa2a40b7bbcc2d
Juliana Sinnott <julianasinnott@outlook.com>
2022-08-28 21:39:58 -0300

    feat: implements success register component

---
+++ src/components/SuccessRegister/index.jsx
@@ -0,0 +1,15 @@
+import { Check } from "phosphor-react";
+import './index.css'
+
+export function SuccessRegister({title}) {
+  return(
+    <div className="success-msg__register">
+      <p> {title} </p>
+      <Check
+        className="check-icon"
+        size={34}
+        color="var(--white)"
+      />
+    </div>
+  )
+}
